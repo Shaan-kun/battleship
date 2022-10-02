6ae89caae5f856ab7e17c11ea7c5cae644722ba3
Shaan <shaan-kun@yandex.ru>
2022-10-02 16:32:34 +0400

    Исправлен баг, если аватарка не выбрана

--- php/reg/signup.php
+++ php/reg/signup.php
@@ -15,26 +15,41 @@
     }
     else
     {
-        $path = '../../images/avatars/'; // путь для сохранения аватарок
-        $avatar = time() . '.' . pathinfo($_FILES['avatar']['name'],  PATHINFO_EXTENSION); // даём аватарке уникальное имя
-
-        if (!move_uploaded_file($_FILES['avatar']['tmp_name'], $path . $avatar))
-        {
-           $_SESSION['message'] = 'Ошибка при загрузке сообщения';
-           header('Location: ../../reg.php');
-        }
-
         $password = md5($password); // хэшируем пароль
 
-        $query = "
-            INSERT INTO User
-                (login, password, email, reg_date, avatar)
-            VALUES
-                ('$login', '$password', '$email', NOW(), '$avatar')
-        ";
+        $query = '';
+
+        if ($_FILES['avatar']['name'])
+        {
+            $path = '../../images/avatars/'; // путь для сохранения аватарок
+            $avatar = time() . '.' . pathinfo($_FILES['avatar']['name'],  PATHINFO_EXTENSION); // даём аватарке уникальное имя
+
+            if (!move_uploaded_file($_FILES['avatar']['tmp_name'], $path . $avatar))
+            {
+               $_SESSION['message'] = 'Ошибка при загрузке сообщения';
+               header('Location: ../../reg.php');
+            }
+
+            $query = "
+                INSERT INTO User
+                    (login, password, email, reg_date, avatar)
+                VALUES
+                    ('$login', '$password', '$email', NOW(), '$avatar')
+            ";
+        }
+        else
+        {
+            $query = "
+                INSERT INTO User
+                    (login, password, email, reg_date)
+                VALUES
+                    ('$login', '$password', '$email', NOW())
+            ";
+        }
 
         mysqli_query($connect, $query);
 
         $_SESSION['message'] = 'Регистрация прошла успешно!';
         header('Location: ../../index.php');
+
     }
