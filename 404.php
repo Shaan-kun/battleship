9b3a87d0149596d0c4dd3a233bb451d6eecc76d9
Shaan <shaan-kun@yandex.ru>
2023-01-20 19:19:47 +0400

    Добавлены настройки перенаправления и обработка 404 ошибки

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>404 Страница не найдена</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+			height: 100vh;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+	</style>
+</head>
+<body>
+<h1>СТРАНИЦА НЕ НАЙДЕНА</h1>
+</body>
+</html>
